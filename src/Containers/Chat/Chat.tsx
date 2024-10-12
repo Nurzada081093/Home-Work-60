448f1703ce646483d0f166c82383d02482ed64f3
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-10-13 01:16:23 +0600

    В файле Chat.tsx была прописана функция которая делает запрос. Далее был выполнен запрос на сообщений с API.

--- src/Containers/Chat/Chat.tsx
+++ src/Containers/Chat/Chat.tsx
@@ -7,6 +7,27 @@ import { useState } from 'react';
 const Chat = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
 
+  const url: string = 'http://146.185.154.90:8000/messages';
+
+  const request = async (url: string, method: string = "GET") => {
+    const response= await fetch(url, {method: method});
+    if (!response.ok) {
+      throw new Error('Network Error: ' + response.status);
+    }
+    return response.json();
+  };
+
+  const getAllMessages = async () => {
+    try {
+      const allMessages = await request(url);
+      console.log(allMessages);
+    } catch (error) {
+      alert(`Error ${error}`);
+    }
+  };
+
+  void getAllMessages();
+
   const addNewMessage = (newMessage: IMessage) => {
     setMessages((prevState) => [...prevState, newMessage]);
   };
